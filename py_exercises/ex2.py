5b96027b0fa843150816506f836c2f4db5ff5cde
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2019-11-08 13:43:59 +0200

    Add implementation that return subarray with max sum

--- py_exercises/ex2.py
+++ py_exercises/ex2.py
@@ -1,14 +1,27 @@
 def max_sum_subvector(vector):
-    if len(set(vector)) == 1:
-        return vector[0]
+    if len(vector) == len([i for i in vector if i < 0]):
+        vector = [i * -1 for i in vector]
 
     if vector:
         current_sum = 0
-        max_sum = vector[0]
+        max_sum = 0
 
-        for element in vector:
-            current_sum = max(current_sum + element, element)
-            max_sum = max(max_sum, current_sum)
+        current_start_index = 0
+        start_index = 0
+        end_index = 0
 
-        return max_sum
-    return 0
+        for index, element in enumerate(vector):
+            if current_sum <= 0:  # max_sum:
+                current_start_index = index
+                current_sum = element
+            else:
+                current_sum += element
+
+            if current_sum > max_sum:
+                max_sum = current_sum
+                start_index = current_start_index
+                end_index = index + 1
+
+        return vector[start_index:end_index]
+
+    return []
